1f438d2bca047158feb00517f64edb4ab735fd03
Jose_Fernandez <jfernand84@gmail.com>
2021-11-16 14:52:07 -0500

    added buzzrxLogo to nav component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p>Navbar coming soon...</p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html {
+  font-family: 'Lato', sans-serif;
+  /* font-family: 'Omnes', sans-serif; */
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
+
+//import global components below
 import App from './App.vue'
 
+//attach global components below
+
+//mount app instance
 createApp(App).mount('#app')
