81cd9f2edeeacbdf30469f1f1376ccd4ad7b7c71
Jose_Fernandez <jfernand84@gmail.com>
2021-11-16 16:07:30 -0500

    navbar-mobile finished - must circle back to ensure reponsiveness

---
+++ src/components/navbar/HamburgerBtn.vue
@@ -0,0 +1,16 @@
+<template>
+  <!-- <p>burger icon coming soon...</p> -->
+  <img src="/assets/hamburger_icon.png" class="hamburger" />
+</template>
+
+<script></script>
+
+<style scoped>
+.hamburger {
+  width: 16.14px;
+  height: 12.25px;
+  padding-right: 1rem;
+  background-position: 334.13px 28px;
+  opacity: 100%;
+}
+</style>
--- src/components/navbar/NavbarMain.vue
+++ src/components/navbar/NavbarMain.vue
@@ -1,13 +1,31 @@
 <template>
+  <navbar>
   <BuzzRxLogo />
+  <HamburgerBtn />
+  </navbar>
 </template>
 
 <script>
 import BuzzRxLogo from './BuzzRxLogo.vue';
+import HamburgerBtn from './HamburgerBtn.vue';
 
 export default {
   components: {
     BuzzRxLogo,
+    HamburgerBtn,
   },
 };
 </script>
+
+<style scoped>
+navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  top: 0px;
+  left: 0px;
+  /* width: 375px; */
+  height: 70px;
+  opacity: 1;
+}
+</style>
