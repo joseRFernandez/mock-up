fd4f942b819ed194c22aa5fbea14ec1594e09211
Jose_Fernandez <jfernand84@gmail.com>
2021-11-18 15:51:05 -0500

    added paragraph to meetaudrey

---
+++ src/components/meetAudrey/TheMeetAudreySection.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>title purple...</div>
+  <div>subtitle...</div>
+  <div>image...</div>
+  <div>quote...</div>
+  <MeetAudreyText />
+</template>
+
+<script>
+import MeetAudreyText from './MeetAudreyText.vue';
+
+export default {
+  components: {
+    MeetAudreyText,
+  },
+};
+</script>
+
+<style scoped></style>
