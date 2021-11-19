90bc597917a00ed3d0cdac4b65caf73f151d46c7
Jose_Fernandez <jfernand84@gmail.com>
2021-11-18 21:00:36 -0500

    signuptext added - NOT COMPLETE - return tomorrow

--- src/components/signup-form/AgreeToVisit.vue
+++ src/components/signup-form/AgreeToVisit.vue
@@ -1,14 +1,27 @@
 <template>
-  <div>Checkbox coming soon...</div>
-  <div class="text">
-    <p>
-      YES, I would like a BuzzRx Representative to visit my pharmacy and
-      occasionally bring FREE
-    </p>
+  <div>
+    <section class="checkbox"></section>
+    <section class="text">
+      <p>
+        YES, I would like a BuzzRx Representative to visit my pharmacy and
+        occasionally bring FREE
+      </p>
+    </section>
   </div>
 </template>
 
 <style scoped>
+div {
+  display: flex;
+}
+.checkbox {
+  width: 13px;
+  height: 13px;
+  background: transparent url('/assets/check-box/check-box.png') 0% 0% no-repeat
+    padding-box;
+  opacity: 1;
+  margin: 3px 0 0 27px;
+}
 p {
   text-align: left;
   font: normal normal normal 14px/18px Lato;
@@ -19,5 +32,7 @@ p {
 .text {
   width: 298px;
   height: 36px;
+  display: inline;
+  margin: 0 22px 0 55px;
 }
 </style>
