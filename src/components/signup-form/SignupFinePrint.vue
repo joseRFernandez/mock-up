8f7dfa2e8b2c1afc41758246cbe27614f05ad620
Jose_Fernandez <jfernand84@gmail.com>
2021-11-18 19:55:26 -0500

    signupfineprint component complete

---
+++ src/components/signup-form/SignupFinePrint.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>
+      By submitting this form, you agree to BuzzRx’s
+      <span>terms of use</span> and <span> policy</span>. By providing BuzzRx
+      with your phone number and email address, you agree to receive
+      informational and other SMS text messages and emails from BuzzRx. Your
+      consent to receive SMS text messages from BuzzRx is not a condition of
+      purchase of BuzzRx products or services. You may opt out of receiving SMS
+      text messages from BuzzRx at any time by replying “STOP”.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+div {
+  width: 329px;
+  height: 147.16px;
+  margin: 23px 24px 0 22px;
+}
+p {
+  text-align: left;
+  font: normal normal normal 14px/16px Lato;
+  letter-spacing: 0px;
+  opacity: 1;
+}
+span {
+  color: #b33cc8;
+}
+</style>
--- src/components/signup-form/TheSignUpForm.vue
+++ src/components/signup-form/TheSignUpForm.vue
@@ -3,14 +3,16 @@
   <div>Input Forms coming soon...</div>
   <div>Agree to Visit coming soon...</div>
   <div>Badge coming soon...</div>
-  <div>Fine Print coming soon...</div>
+  <SignupFinePrint />
   <MakeAWishLogo />
 </template>
 <script>
+import SignupFinePrint from './SignupFinePrint.vue';
 import MakeAWishLogo from './MakeAWishLogo.vue';
 export default {
   components: {
     MakeAWishLogo,
+    SignupFinePrint,
   },
 };
 </script>
