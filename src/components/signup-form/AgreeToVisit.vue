2048768a23c4819a1bcb0ab62c3fa0cb8552571a
Jose_Fernandez <jfernand84@gmail.com>
2021-11-18 20:40:22 -0500

    agreeText added

---
+++ src/components/signup-form/AgreeToVisit.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>Checkbox coming soon...</div>
+  <div class="text">
+    <p>
+      YES, I would like a BuzzRx Representative to visit my pharmacy and
+      occasionally bring FREE
+    </p>
+  </div>
+</template>
+
+<style scoped>
+p {
+  text-align: left;
+  font: normal normal normal 14px/18px Lato;
+  letter-spacing: 0px;
+  color: #262626;
+  opacity: 1;
+}
+.text {
+  width: 298px;
+  height: 36px;
+}
+</style>
--- src/components/signup-form/TheSignUpForm.vue
+++ src/components/signup-form/TheSignUpForm.vue
@@ -1,17 +1,19 @@
 <template>
   <div>Sign up text coming soon...</div>
   <div>Input Forms coming soon...</div>
-  <div>Agree to Visit coming soon...</div>
+  <AgreeToVisit />
   <SignUpBadge />
   <SignupFinePrint />
   <MakeAWishLogo />
 </template>
 <script>
+import AgreeToVisit from './AgreeToVisit.vue';
 import SignUpBadge from './SignUpBadge.vue';
 import SignupFinePrint from './SignupFinePrint.vue';
 import MakeAWishLogo from './MakeAWishLogo.vue';
 export default {
   components: {
+    AgreeToVisit,
     SignUpBadge,
     MakeAWishLogo,
     SignupFinePrint,
