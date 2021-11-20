48e9e9e827d379136378eb1d9b5d017e470706a1
Jose_Fernandez <jfernand84@gmail.com>
2021-11-20 15:59:24 -0500

    header tablet complete

--- src/components/header/HeaderImageMobile.vue
+++ src/components/header/HeaderImageMobile.vue
@@ -4,12 +4,19 @@
 
 <style scoped>
 .header-mobile-image {
-  top: 199px;
-  left: 0px;
-  width: 375px;
+  width: 100%;
   height: 155px;
   background: transparent url('/assets/mobile-header/mobile-header.png') 0% 0%
     no-repeat padding-box;
   opacity: 1;
+}
+@media screen and (max-width: 768px) {
+  .header-mobile-image {
+    width: 768px;
+    height: 334px;
+    background: transparent url('/assets/mobile-header/mobile-header@2x.png') 0%
+      0% no-repeat padding-box;
+      background-size: cover;
+  }
 }
 </style>
