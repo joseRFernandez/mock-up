239d26d149a6aa49ed3bff8ebe5869daa6734028
Jose_Fernandez <jfernand84@gmail.com>
2021-11-20 15:17:12 -0500

    hamburger tablet good

--- src/components/navbar/HamburgerBtn.vue
+++ src/components/navbar/HamburgerBtn.vue
@@ -1,5 +1,9 @@
 <template>
-  <img src="/assets/hamburger_icon.png" class="hamburger" />
+  <div class="navbar-hamburger-container">
+    <div class="navbar-inner-hamburger-container">
+      <img src="/assets/hamburger_icon.png" class="hamburger" />
+    </div>
+  </div>
 </template>
 
 <script></script>
@@ -8,8 +12,25 @@
 .hamburger {
   width: 16.14px;
   height: 12.25px;
-  background-position: 334.13px 28px;
   opacity: 100%;
-  margin-right: 24px;
+}
+@media screen and (min-width: 765px) {
+  .hamburger {
+    width: 24.81px;
+    height: 16.54px;
+  }
+  .navbar-hamburger-container {
+    width: 544px;
+    height: 50px;
+    padding: 17px 23px 8px 0px;
+  }
+  .navbar-inner-hamburger-container {
+    width: 33px;
+    height: 33px;
+    padding: 8.27px 4.14px;
+    text-align: center;
+    padding: 9px 13px 8px 498px;
+  }
+
 }
 </style>
--- src/components/navbar/TheNavbar.vue
+++ src/components/navbar/TheNavbar.vue
@@ -24,9 +24,6 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  height: 38px;
-  opacity: 1;
-  padding: 15px 0 17px 8px;
 }
 .navbar-buzz-logo {
   width: 154.2px;
@@ -39,5 +36,11 @@ export default {
   background-position: 8.21px 15.62px;
   opacity: 1;
   margin-left: 8.21px;
+}
+
+@media screen and (max-width: 765px) {
+ .navbar-container {
+
+ }
 }
 </style>
