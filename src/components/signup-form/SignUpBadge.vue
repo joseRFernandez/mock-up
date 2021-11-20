984e5530e46cdca5be99f6a6fc0f26a1045d0f54
Jose_Fernandez <jfernand84@gmail.com>
2021-11-20 18:23:28 -0500

    signup badge tablet ready

--- src/components/signup-form/SignUpBadge.vue
+++ src/components/signup-form/SignUpBadge.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="badge">
-    <p>Sign Up To Give Back</p>
+  <div class="badge-container">
+    <div class="badge">
+      <p>Sign Up To Give Back</p>
+    </div>
   </div>
 </template>
 
 <script></script>
 
 <style scoped>
+.badge-container {
+  margin-left: 204px;
+}
 .badge {
   width: 332px;
   height: 40px;
@@ -21,5 +26,10 @@ p {
   line-height: 19px;
   padding: 9.15px 64.22px 12.85px 64.22px;
   color: #ffffff;
+}
+@media screen and (max-width: 768px) {
+  .badge {
+    width: 360px;
+  }
 }
 </style>
