7307462983a024cb3de4b4edc530d3670fa26197
Jose_Fernandez <jfernand84@gmail.com>
2021-11-21 07:59:35 -0500

    going to scale to tablet now

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
      <TheMeetAudreySection />
     <TheMAWPartnershipSection />
     <TheGetACardSection />
-    <!-- <TheCeoMessage />
-    <TheFooter />  -->
+    <TheCeoMessage />
+    <TheFooter />
   </body>
 </template>
 
@@ -15,11 +15,11 @@
 import TheNavbar from './components/navbar/TheNavbar.vue';
 import TheHeader from './components/header/TheHeader.vue';
 import TheSignUpForm from './components/signup-form/TheSignUpForm.vue';
-// import TheCeoMessage from './components/ceo-message/TheCeoMessage.vue';
+import TheCeoMessage from './components/ceo-message/TheCeoMessage.vue';
 import TheGetACardSection from './components/getACard/TheGetACardSection.vue';
 import TheMAWPartnershipSection from './components/MAW-partnership/TheMAWPartnershipSection.vue';
 import TheMeetAudreySection from './components/meetAudrey/TheMeetAudreySection.vue';
-// import TheFooter from './components/footer/TheFooter.vue';
+import TheFooter from './components/footer/TheFooter.vue';
 
 export default {
   name: 'App',
@@ -27,8 +27,8 @@ export default {
     TheNavbar,
     TheHeader,
     TheSignUpForm,
-    // TheFooter,
-    // TheCeoMessage,
+    TheFooter,
+    TheCeoMessage,
     TheGetACardSection,
     TheMAWPartnershipSection,
     TheMeetAudreySection,
